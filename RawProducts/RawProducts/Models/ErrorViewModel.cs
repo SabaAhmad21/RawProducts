94c61989de98d721e431e0fdcc4ea110f1ff2a61
Saba Ahmad <sabaahmad821@gmail.com>
2023-12-13 16:44:36 +0500

    StartUp

---
+++ RawProducts/RawProducts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RawProducts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
